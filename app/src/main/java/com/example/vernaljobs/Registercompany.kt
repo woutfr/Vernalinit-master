42ba40b9ae843b59abf2ea81b153165aa418214e
wout <wout_frijters@hotmail.com>
2019-07-18 17:04:46 +0200

    5

--- app/src/main/java/com/example/vernaljobs/Registercompany.kt
+++ app/src/main/java/com/example/vernaljobs/Registercompany.kt
@@ -10,39 +10,30 @@ import kotlinx.android.synthetic.main.activity_register_company.*
 import java.util.regex.Matcher
 import java.util.regex.Pattern
 import android.util.Patterns
-
+import androidx.core.content.ContextCompat.startActivity
 
 
 class Registercompany : AppCompatActivity() {
 
+    lateinit var textInputPassword: EditText
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_company)
+
+        fun registerBack(view: View) {
+
+            // Create an Intent to start the second activity
+            val registerIntent = Intent(this, MainActivity::class.java)
+
+            // Start the new activity.
+            startActivity(registerIntent)
+        }
+
+
     }
-
-    fun registerBack(view: View) {
-
-        // Create an Intent to start the second activity
-        val registerIntent = Intent(this, MainActivity::class.java)
-
-        // Start the new activity.
-        startActivity(registerIntent)
-    }
-
-//    fun isValidPassword(view: View) : Boolean {
-//        password?.let {
-//            val passwordPattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{4,}$"
-//            val passwordMatcher = Regex(passwordPattern)
-//
-//            return passwordMatcher.find(passwordPattern) != null
-//        } ?: return false
-//    }
-
-
-
 
 }
-
-
 
 
