5a241386cf39ae93fafdf8feeebe2e7d2b06ad90
wout <wout_frijters@hotmail.com>
2019-07-22 02:06:27 +0200

    7

--- app/src/main/java/com/example/vernaljobs/Registercompany.kt
+++ app/src/main/java/com/example/vernaljobs/Registercompany.kt
@@ -3,20 +3,41 @@ package com.example.vernaljobs
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
 
 
 class Registercompany : AppCompatActivity() {
-
-//    lateinit var textInputPassword: EditText
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_company)
 
+        lateinit var option: Spinner
+//        lateinit var result: TextView
 
-        }
+        option = findViewById(R.id.spinner) as Spinner
+//        result = findViewById(R.id.Spinner_text) as TextView
+
+        val options = arrayOf("Delft","Elsewhere")
+
+        option.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_activated_1, options)
+
+//        option.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+//            override fun onNothingSelected(p0: AdapterView<*>?) {
+//                result.text = "Please select an option"
+//            }
+
+//            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+//                result.text = options.get(p2)
+//            }
+
+
+
+    }
+
     fun registerBack(view: View) {
 
         // Create an Intent to start the second activity
@@ -27,6 +48,9 @@ class Registercompany : AppCompatActivity() {
 
     }
 
+
 }
+
+
 
 
